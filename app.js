5332f1b0adf91028c275e65add682ef03741f89b
AlessandroLF <lombardoalessandro25@hotmail.com>
2023-04-23 21:58:16 -0600

    MVP

---
+++ app.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const index = require("./index");
+
+var dic = {};
+
+const server = http.createServer((req, res) => {
+    console.log("Request: " + req.url);
+    console.log(dic);
+    let contentType = "text/html";
+
+    switch(req.url){
+
+      case "/":
+        index(req, res, dic);
+        break;
+  
+      default:
+        let extname = path.extname(req.url);
+        switch (extname) {
+          case ".js":
+            contentType = "text/javascript";
+            break;
+          case ".css":
+            contentType = "text/css";
+            break;
+          case ".json":
+            contentType = "application/json";
+            break;
+          case ".png":
+            contentType = "image/png";
+            break;
+          case ".jpg":
+            contentType = "image/jpg";
+            break;
+        }
+  
+        if (contentType == "text/html" && extname == "") req.url += ".html";
+  
+        fs.readFile(path.join(__dirname, 'public', req.url), (err, content) => {
+          if (err) {
+            if (err.code == "ENOENT") {
+              fs.readFile(path.join("public", "404.html"), (err, content) => {
+                  res.writeHead(404, { "Content-Type": "text/html" });
+                  res.end(content, "utf8");
+                }
+              );
+            }
+            else{
+              res.writeHead(500);
+              res.end(`Server Error: ${err.code}`);
+            }
+          } else {
+            res.writeHead(200, { "Content-Type": contentType });
+            res.end(content, "utf8");
+          }
+        });
+      
+    }
+  });
+  
+  const PORT = process.env.PORT || 5000;
+  
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
