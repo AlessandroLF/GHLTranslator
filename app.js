5fab054d56cbe15bde623429ffa762ca89c81b19
AlessandroLF <lombardoalessandro25@hotmail.com>
2023-04-29 04:23:17 -0600

    Router bug fix

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const server = http.createServer((req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.writeHead(200, { "Content-Type": "text/javascript" });
         fs.readFile(path.join(__dirname, 'translator.js'), (content) => {
-          res.write('alert("Ok");');
           res.end(content);
         });
         break;
