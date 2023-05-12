b379dc9f70d030e796e832f3458ff15e77c842de
AlessandroLF <lombardoalessandro25@hotmail.com>
2023-05-12 02:10:39 -0600

    admin page fix

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const server = http.createServer((req, res) => {
 
       case "adminlogin":
         res.writeHead(200, { "Content-Type": "application/json" });
-        res.end({'res':'true'});
+        res.end("{'res':'true'}");
       break
 
       case "login":
